5a896b1fc9965c6795df83d50717b3c6afb6d2b1
Andrea Gabriele <Andrea Gabriele>
2015-12-15 17:24:27 +0100

    sprint1

--- js/app.js
+++ js/app.js
@@ -9,16 +9,34 @@ var SantaModel = {
    * and sets the first one as the current one 
    */
    init : function(list){
+       
+       this.list=requests;
+       this.getCurrentRequest();
+       
    },
   
    /* It moves "current" to the next request */
    next : function (){
+      
+    
+    
    },
   
    /* Returns the current request. 
     * If all requests have been processed (there is no current one), it returns null 
     */
    getCurrentRequest : function () {
+       
+       var current=0;
+       var array = [];
+       array.push(this.list[current]);
+       if(this.list[current]==undefined){
+            return null;
+       }
+       
+       this.list = array;
+       return this.list;
+       
    },  
     
    /* Packs the given item if it fulfills the current request.       
@@ -26,6 +44,57 @@ var SantaModel = {
     * returns 0 if the given item does not fulfill the request
     */
    pack : function(item) {
+       
+       for(var k=0; k<this.list.length; k++){
+            if(this.list[k].answer==item){
+                return 1;
+            }else{
+                return 0;
+            }
+       }
+       
    }      
   
 };
+
+var Controller = {
+    
+    init : function () {
+        SantaModel.init();
+        View.init();
+    },
+    
+    getRequests : function () {
+        return SantaModel.list;
+    }
+};
+
+var View = {
+    
+    init : function () {
+        var array = Controller.getRequests();
+        
+        for(var i=0; i< array.length; i++){
+            $(".question").append(array[i].question);
+            $(".question-items").append("<li>"+array[i].options+"</li>");
+        }
+    }
+};
+
+
+$(document).ready(function(){
+    
+   Controller.init();
+   var correct=0;
+    
+    $(".question-items").click(function(){
+        SantaModel.next();
+        correct=correct+SantaModel.pack();
+        if(SantaModel.getCurrentRequest()==null){
+            $(".question").html("");
+            $(".question-items").html("");
+            $(".result").html("Total points: "+ correct );
+        }
+    })
+    
+});
